0339d57f912068a1ec4cf711876a82a318606fc9
Adam <adammoore118@outlook.com>
2021-10-12 11:48:04 -0400

    commit tree f47346434956d0f4db061dd0716a0e0df8347bce

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from '/home/adammoore/Development/phase-2-code/react-hooks-import-export-lab/src/components/About.js'
+import Home from '/home/adammoore/Development/phase-2-code/react-hooks-import-export-lab/src/components/Home.js'
+import NavBar from '/home/adammoore/Development/phase-2-code/react-hooks-import-export-lab/src/components/NavBar.js'
 
 function App() {
   return (
